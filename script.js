c832583d9ed9a2eab56eb8245c7b2daa9f6f0c87
JhonRexGarduque <garduejhonrex@gmail.com>
2023-12-13 19:35:57 +0800

    final

---
+++ script.js
@@ -0,0 +1,26 @@
+// JavaScript 
+document.addEventListener('DOMContentLoaded', function () {
+    // Scroll bar
+    document.querySelectorAll('nav a').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+  
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+  
+  
+    
+    //Simple form submission event
+  
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+    
+    //Form
+      alert('Form submitted!');
+      });
+    });
+    
